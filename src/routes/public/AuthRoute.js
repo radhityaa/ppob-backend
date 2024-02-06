56f8cee783341325710695fc5b5a12fbb33b1e2e
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-02-06 11:58:29 +0700

    feature/login

--- src/routes/public/AuthRoute.js
+++ src/routes/public/AuthRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { registerValidation } from '../../validation/AuthValidation.js'
+import { loginValidation, registerValidation } from '../../validation/AuthValidation.js'
 import validation from '../../validation/validation.js'
-import { RegisterController } from '../../controllers/AuthController.js'
+import { LoginController, RegisterController } from '../../controllers/AuthController.js'
 
 const authRoute = express.Router()
 
 authRoute.post('/api/auth/register', registerValidation, validation, RegisterController)
+authRoute.post('/api/auth/login', loginValidation, validation, LoginController)
 
 export default authRoute
